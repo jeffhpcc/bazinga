73515d234d0d5c371a11dfeecbf9ae81272d104b
randymay <randy.may@blaazinsoftware.com>
2014-09-26 13:46:33 -0400

    Create Medication class

---
+++ bazingalocal/Medication.swift
@@ -0,0 +1,14 @@
+//
+//  Medication.swift
+//  bazingalocal
+//
+//  Created by Randy May on 2014-09-26.
+//  Copyright (c) 2014 pcchack. All rights reserved.
+//
+
+import Foundation
+
+class Medication: NSObject {
+    @NSManaged var name: NSString
+    @NSManaged var notes: NSString
+}
